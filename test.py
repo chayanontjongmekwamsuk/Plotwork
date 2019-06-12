d1f33a487c70d9905c798647de54cd3cd6878b7e
Chayanont Jongmekwamsuk <chayanont.jon@student.mahidol.ac.th>
2019-06-12 22:07:50 +0700

    test plot completed

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 data = pd.read_csv("test1.csv")
-
-data.plot()
+print(data.iloc[1, :])
+plt.plot(data.iloc[1, :])
+plt.plot(data.iloc[2, :])
+plt.plot(data.iloc[3, :])
 plt.show()
